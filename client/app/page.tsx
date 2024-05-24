7c390641002ad81b8daa5e4919fc8f611a44dd6c
Ashish <ashish74624@gmail.com>
2024-05-24 17:42:32 +0530

    home page done

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,41 @@
-import Image from "next/image";
+import { Vortex } from "@/components/ui/vortex";
+import Link from "next/link";
+import { FlipWords } from "@/components/ui/flip-words";
+import { Button } from "@/components/ui/moving-border";
 
 export default function Home() {
+  const words = ["better", "cute", "beautiful", "modern"];
   return (
-    <></>
+    <div className=" mx-auto w-screen  h-screen overflow-hidden">
+      <Vortex
+        backgroundColor="black"
+        rangeY={800}
+        particleCount={800}
+        className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
+      >
+        <h1 className="text-white text-2xl md:text-6xl font-bold text-center">
+          Image Uploader
+        </h1>
+        <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
+          <Link href={'/login'}>
+
+          <Button borderRadius="1.75rem" className="bg-white text-xl dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800">
+            Login
+          </Button>
+          </Link>
+          <Link href={'/signup'}>
+
+          <Button borderRadius="1.75rem" className="bg-white text-xl dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800">
+            Sign Up
+          </Button>
+          </Link>
+        </div>
+        <div className="text-2xl mt-4 mx-auto font-normal text-white">
+          Build
+          <FlipWords words={words} /> 
+          websites with Image Uploader
+        </div>
+      </Vortex>
+    </div>
   );
 }
